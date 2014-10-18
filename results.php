78213ebb8615c10c2975db66dd83c478abcebc7c
aflatht65 <aflatht65@gmail.com>
2014-10-18 09:28:11 +0300

    initial commit/form

---
+++ results.php
@@ -0,0 +1,9 @@
+<?php
+$movies = array(
+   'Drama' => array('The Shawshank Redemption', 'The Green Mile', 'Requiem for a a Dream'),
+   'Comedy' => array('The Hangover', 'Knocked Up', 'Due Date'),
+   'Horror' => array('Halloween', 'The Conjuring', 'The Shining'),
+);
+
+echo $movies['Drama']['1'];
+?>
